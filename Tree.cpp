a390635a7ee8a27d99e40042e8c80cd7da16b99e
Lauren <lamc8684@colorado.edu>
2015-04-25 20:17:10 -0600

    wrote function to flip a coin and randomly choose heads or tails

--- Tree.cpp
+++ Tree.cpp
@@ -5,6 +5,23 @@ Tree::Tree()
     //ctor
 }
 
+void Tree::flipcoin(){
+//0 is Tails and 1 is Heads
+//random number generator indicates heads or tails
+int flip=rand % 1+0;
+if (flip==1) //heads
+    if(tracker->left!=NULL)
+        tracker=tracker->left;
+    else //at the leaf
+        cout<<"No more available flips"<<endl;
+else //tails
+    if(tracker->right!=NULL)
+        tracker=tracker->right;
+    else //at the leaf
+        cout<<"No more available flips"<<endl;
+
+}
+
 Tree::~Tree()
 {
     //dtor
